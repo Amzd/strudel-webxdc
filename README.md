7402ffe4cbf63fe63a90d077846979916f8cde5d
Casper Zandbergen <info@casperzandbergen.nl>
2026-04-09 18:29:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Webxdc App Template
+
+Template project for building [Webxdc](https://webxdc.org/) applications
+
+## Getting started
+
+Make sure you have the following installed:
+
+- [`NodeJS`](https://nodejs.org/)
+- [`pnpm`](https://pnpm.io/)
+
+Some commonly used scripts:
+
+- Install deps (usually when initially cloning the repo):
+
+  ```sh
+  pnpm install
+  ```
+
+- Perform some code checks:
+
+  ```sh
+  pnpm run check
+  ```
+
+- Start the dev server:
+
+  ```sh
+  pnpm run dev
+  ```
+
+- Start the Webxdc emulator (must have the dev server running already):
+
+  ```sh
+  pnpm run webxdc:emulator
+  ```
+
+- Build the application for distribution:
+
+  ```sh
+  pnpm run build
+  ```
+
+## License
+
+[MIT](./LICENSE)
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
+import pluginJs from '@eslint/js'
+import globals from 'globals'
+import pluginTs from 'typescript-eslint'
+
+export default pluginTs.config(
+	includeIgnoreFile(fileURLToPath(new URL('.gitignore', import.meta.url))),
+	includeIgnoreFile(
+		fileURLToPath(new URL('.git/info/exclude', import.meta.url))
+	),
+	{ files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'] },
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	pluginJs.configs.recommended,
+	{
+		extends: pluginTs.configs.recommended,
+		rules: {
+			'@typescript-eslint/array-type': ['warn', { default: 'generic' }],
+			// Allow unused vars if prefixed with `_` (https://typescript-eslint.io/rules/no-unused-vars/)
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	}
+)
